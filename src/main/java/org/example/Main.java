cd4d29126e3b34b6c16a9b0b2e2fc0a74f6b24b4
mpuss <herdiseptanto2017@gmail.com>
2023-10-14 23:58:25 +0700

    add ModelData.java or list class data name-lang

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,31 @@
 package org.example;
 
-public class Main{
-    static Translation translation = new Translation();;
+public class Main {
+    static Translation translation = new Translation();
+    static ModelData modelData = new ModelData();
+    ;
     static private String resourceText, outLang, targetLang;
+
     public static void main(String[] args) {
 //        translation.setApiUrl("Hello%20World!","en","id");
         if (args.length == 4 && args[0].equals("-f") || args[0].equals("--from")) {
             resourceText = String.valueOf(args[1]);
             outLang = String.valueOf(args[2]);
             targetLang = String.valueOf(args[3]);
-            translation.setApiUrl("from",resourceText,outLang,targetLang);
+            translation.setApiUrl("from", resourceText, outLang, targetLang);
         } else if (args.length == 3 && args[0].equals("-t") || args[0].equals("--to")) {
             resourceText = String.valueOf(args[1]);
             targetLang = String.valueOf(args[2]);
-            translation.setApiUrl("to",resourceText,"Autodetect",targetLang);
+            translation.setApiUrl("to", resourceText, "Autodetect", targetLang);
+        } else if (args.length == 1 && args[0].equals("-l") || args[0].equals("--list")) {
+            modelData.getData();
         } else if (args[0].equals("-h") || args[0].equals("--help")) {
             System.out.println("rasan-translate (version 1.0, revision 1)");
             System.out.println("Usage:\n" +
                     " kuncen [OPTIONS]...[VALUES]\t\n" +
                     "  -f, --from 'source' [from] [to]    Specify source-lang (id for Indonesian) and target-lang (eng for English).\n" +
                     "  -t, --to   'source' [to]    Specifiy target-lang (id for Indonesian) will automatically detect the source-lang.\n" +
+                    "  -l, --list   'acronym,name'    List target-lang (id for Indonesian) will loop format data : acronym,name.\n" +
                     "  -h, --help          Display usage,options and help.\n");
         } else {
             System.out.println("rasan: missing operand\n" +
