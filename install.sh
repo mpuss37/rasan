7c489cd52dc75a3469ac3a6d70d8373a80dfc1b4
mpuss <herdiseptanto2017@gmail.com>
2023-10-20 21:01:17 +0700

    add ()  for option menu for get can zero param

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
-full_path=$('pwd' /gradlew jar)
+./gradlew jar
+check_build=$(cd build)
 current_path=$('pwd')
 check_code_bash=$(cat $HOME/.bashrc | grep rasan)
-if [ $full_path ]; then
   echo "berhasil build $full_path ✓"
   echo "hasil build $current_path build/libs ✓"
-else
-  echo "gagal build."
-fi
 if [[ $check_code_bash != *rasan* ]]; then
-echo "alias rasan='java -jar $current_path kuncen-1.0-SNAPSHOT.jar' " >> $HOME/.bashrc
+echo "alias rasan='java -jar $current_path/build/libs/kuncen-1.0-SNAPSHOT.jar' " >> $HOME/.bashrc
   echo "add .bashrc (global) ✓"
 else
   echo "cannot add .bashrc"
